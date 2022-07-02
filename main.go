20b2778332a1bc718c10b86ad2b3225c7f4d1e11
hungdongngoc <hungdongngoc@vccorp.vn>
2022-07-02 10:28:47 +0700

    init

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hi")
+	})
+
+	log.Println("Starting server at :8081")
+	log.Fatal(http.ListenAndServe(":8081", nil))
+
+}
